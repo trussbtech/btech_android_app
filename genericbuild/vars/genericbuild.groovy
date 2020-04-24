3d63ee6f703089d6463e8138409f3d0b4cfcf00d
Tyrone Russ <tyronerussaws.@gmail.com>
2020-04-24 00:42:59 -0700

    Number 14

---
+++ genericbuild/vars/genericbuild.groovy
@@ -0,0 +1,26 @@
+def call(Map config=[:]) {
+node {
+    stage('SCM') {
+		echo 'Gathering code from version control'
+		git branch: '${branch}', url: 'https://github.com/trussbtech/btech_android_app.git'
+    }
+    stage('Build') {
+		echo 'Building Master branch'
+		sh './gradlew build '
+		echo 'The build stage passed...'
+		releasenotes(changes:"true")
+	}	
+	 
+	stage('Tests') {
+		echo 'The testing stage passed...'
+		//Start all the existing tests in the test package 
+	}
+	
+    stage('Test') {
+		echo 'Testing At master branch'
+    }
+    stage('Deploy') {
+        echo 'Deploying....'
+    }
+}
+}
