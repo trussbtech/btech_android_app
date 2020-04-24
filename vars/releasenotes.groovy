f484f733c747c0f549a633035f30b9c46443c710
Tyrone Russ <tyronerussaws.@gmail.com>
2020-04-24 00:27:50 -0700

    Number 13

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -14,33 +14,32 @@ def call(Map config=[:]) {
                 writer ->
                     dir.eachFileRecurse(FileType.ANY) { file ->
                         if (file.isDirectory()) {
-                   writer.writeLine(file.name); 
-                }
-                else
-                { 
-                    writer.writeLine('\t' + file.name + '\t' + file.length());
-                }
-           }
-
-        def date = new Date()
-        def sdf  = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-        echo "Date and Time is:  " + sdf.format(date)
-
-        echo "Build number is: ${BUILD_NUMBER}";
-
-        def changeLogSets = currentBuild.changeSets
-
-        for (change in changeLogSets) {
-            def entries = change.items;
-                for (entry in entries) {
-                    writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}")
-                    for (file in entry.affectedFiles) {
-                        writer.writeLine(" ${file.editType.name} ${file.path}")
+                            writer.writeLine(file.name);
+                        } else {
+                            writer.writeLine('\t' + file.name + '\t' + file.length());
+                        }
                     }
-                }
-        }
 
-    if(config.changes != "false") {
-    echo "Changes"
-    }
+                    def date = new Date()
+                    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+                    echo "Date and Time is:  " + sdf.format(date)
+
+                    echo "Build number is: ${BUILD_NUMBER}";
+
+                    def changeLogSets = currentBuild.changeSets
+
+                    for (change in changeLogSets) {
+                        def entries = change.items;
+                        for (entry in entries) {
+                            writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}")
+                            for (file in entry.affectedFiles) {
+                                writer.writeLine(" ${file.editType.name} ${file.path}")
+                            }
+                        }
+                    }
+
+                    if(config.changes != "false") {
+                        echo "Changes"
+                    }
+       }
 }
