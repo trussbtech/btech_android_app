2a21e4cd4780fa73731b6c9fc0ad524d20d3dee4
Tyrone Russ <tyronerussaws.@gmail.com>
2020-04-23 23:37:11 -0700

    Number 8

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -22,7 +22,7 @@ def call(Map config=[:]) {
            }
     }
     def date = new Date()
-    def sdf  = new SimpleDateFormet("MM/dd/yyyy HH:mm:ss")
+    def sdf  = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
     echo "Date and Time IS: = " + sdf.format(date)
 
     if(config.changes != "false") {
